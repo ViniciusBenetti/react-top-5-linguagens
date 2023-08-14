233f194b57079c3fc56862b1ccafc8c3c2e3483d
ViniciusBenetti <makako1289@gmail.com>
2023-08-14 20:48:43 -0300

    html e css

---
+++ src/components/Linguagens.js
@@ -0,0 +1,16 @@
+function Linguagens(){
+
+    return(
+        <ol>
+            <li> Python - <a  href="https://go.hotmart.com/W85584839B">comprar</a></li>
+            <li>  JavaScript - <a href="https://go.hotmart.com/O85257901N">comprar</a></li>
+            <li>  Java  - em breve</li>
+            <li>  C++ - <a href="https://go.hotmart.com/G85793664J">comprar</a></li>
+            <li>  C# - em breve</li>      
+        </ol>
+    )
+
+
+}
+
+export default Linguagens
---
+++ src/components/Titulo.js
@@ -0,0 +1,15 @@
+function Titulo(){
+
+    return(
+
+        <div id="título">
+
+    <h1><b>Top cinco linguagens de programação</b></h1>
+
+
+        </div>
+
+    )
+}
+
+export default Titulo;
